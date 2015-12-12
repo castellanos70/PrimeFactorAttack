b7b0392325e2e6cf34f7c73fcbbe84f3a97f9ce0
RhodesToRome <rhodesgamer@yahoo.com>
2015-12-11 22:49:54 -0700

    Added logo to replace Java log in top left. Made the cursor have a new look in game window.

--- src/PrimeFactorAttack/PrimeFactorAttack.java
+++ src/PrimeFactorAttack/PrimeFactorAttack.java
@@ -3,8 +3,12 @@ package PrimeFactorAttack;
 import javax.swing.*;
 
 import java.awt.Color;
-import java.awt.Container;  //On which to add buttons and JPanel
-
+import java.awt.Container;//On which to add buttons and JPanel
+import java.awt.Cursor;
+import java.awt.Image;
+import java.awt.Insets;
+import java.awt.Point;
+import java.awt.Toolkit;
 //Listen to button clicks
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -132,6 +136,15 @@ public class PrimeFactorAttack extends JFrame implements ActionListener
     fullPanel.setLocation(0, 0);
 
     welcomeScreen = new WelcomeScreen(fullPanel);
+
+    Toolkit toolkit = Toolkit.getDefaultToolkit();
+
+    Image image = new ImageIcon(Data.resourcePath + "cursor1.png").getImage();
+    Cursor c = toolkit.createCustomCursor(image, new Point(0, 0), "img");
+    this.getRootPane().setCursor(c);
+
+    Image logoImage = new ImageIcon(Data.resourcePath + "logo10.png").getImage();
+    setIconImage(logoImage);
 
 
     int width = 752;
